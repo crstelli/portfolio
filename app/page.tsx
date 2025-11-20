5b15aa738f9fcf226e0ab727160ca2ff4263a749
Giuseppe <cresctelli@gmail.com>
2025-11-20 20:45:59 +0100

    Started implementing custom animations for tech stack

--- app/page.tsx
+++ app/page.tsx
@@ -131,10 +131,45 @@ export default function page() {
           tecnologie con cui lavoro in modo fluido e che uso per costruire
           progetti scalabili e affidabili.
         </Subtitle>
-        <div className="flex items-center justify-center mt-10">
-          <div className="border p-1 rounded-md border-neutral-900 bg-neutral-950">
-            <div className="relative aspect-square w-[40px]">
+        <div className="flex items-center justify-center gap-4 mt-10">
+          <div className="group overflow-hidden border px-2 py-1 rounded-md flex items-center border-neutral-900 bg-neutral-950">
+            <div className="relative aspect-square w-10">
               <Image src="techstack-logos/tailwindcss.svg" alt="logo" fill />
+            </div>
+
+            <div
+              className="
+      ml-0 group-hover:ml-2
+      max-w-0 group-hover:max-w-[152px]
+      
+      transition-[max-width,margin-left]
+      duration-[500ms,0ms]
+      ease-in
+      delay-[0ms,300ms]
+      flex overflow-hidden
+    "
+            >
+              <h5
+                className="
+        opacity-0 translate-x-full
+        group-hover:opacity-100 group-hover:translate-x-0
+        duration-300 whitespace-nowrap
+      "
+              >
+                TailwindCSS
+              </h5>
+            </div>
+          </div>
+
+          <div className="group cursor-pointer overflow-hidden border px-2 py-1 rounded-md flex items-center border-neutral-900 bg-neutral-950">
+            <div className="relative aspect-square w-10">
+              <Image src="techstack-logos/nextjs.svg" alt="logo" fill />
+            </div>
+
+            <div className="max-w-0 overflow-hidden group-hover:max-w-[500px] transition-[max-width, margin-left] duration-[500ms, 0ms] delay-[0ms, 500ms] ml-0 group-hover:ml-3 flex">
+              <h5 className="opacity-0 translate-x-full group-hover:opacity-100 group-hover:translate-x-0 duration-500 whitespace-nowrap">
+                NextJS
+              </h5>
             </div>
           </div>
         </div>
