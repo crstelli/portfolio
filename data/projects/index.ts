098a8e4142d26e48ba135d54955a8c86379dc293
Giuseppe <cresctelli@gmail.com>
2025-11-22 08:04:44 +0100

    Added last project images

--- data/projects/index.ts
+++ data/projects/index.ts
@@ -1,5 +1,6 @@
 import { project1 } from "./project-1";
 import { project2 } from "./project-2";
 import { project3 } from "./project-3";
+import { project4 } from "./project-4";
 
-export const projects = [project1, project2, project3];
+export const projects = [project1, project2, project3, project4];
---
+++ data/projects/project-4.ts
@@ -0,0 +1,40 @@
+import type { Project } from "./type";
+
+export const project4: Project = {
+  slug: "food-truck",
+  stack: "Full Stack",
+  date: 2025,
+  name: "Shoppy Website",
+  description:
+    "Shoppy is a modern e-commerce platform where users can order a variety of digital and physical products, filter them by price and status, and track their orders.",
+  technologies: [
+    "Next.js",
+    "React",
+    "TailwindCSS",
+    "Typescript",
+    "Supabase",
+    "Vercel",
+    "Auth.js",
+    "date-fns",
+    "lucide-react",
+    "react-hot-toast",
+    "react-range",
+  ],
+  features: ["Responsive Design", "Login with Google", "Order Tracking"],
+  github: "https://github.com/crstelli/food-truck",
+  images: [
+    "/food-truck/preview.png",
+    "/food-truck/image-1.png",
+    "/food-truck/image-2.png",
+    "/food-truck/image-3.png",
+    "/food-truck/image-4.png",
+    "/food-truck/image-5.png",
+  ],
+
+  overview: "overview",
+  technical_decisions: "technical_decisions",
+  challenges: "challenges",
+  key_features: [{ title: "title1", content: "content" }],
+  future_improvements: [{ title: "title2", content: "content" }],
+  lessons: [{ title: "title3", content: "content" }],
+};
