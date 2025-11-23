18ac55c05a8cf0c0c0326d473b61ab0cf283ac6d
Giuseppe <cresctelli@gmail.com>
2025-11-23 08:55:14 +0100

    Built a profile card in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,6 +4,9 @@ import { Title } from "@/components/Title";
 
 import Content from "./content.mdx";
 import { Button } from "@/components/Button";
+import Link from "next/link";
+import { GitHub, Linkedin } from "react-feather";
+import { links } from "@/data/links";
 
 export default function page() {
   return (
@@ -20,15 +23,42 @@ export default function page() {
           <h1 className="text-4xl ml-4 font-bold text-white">
             Meet <span className="text-primary">Giuseppe</span>
           </h1>
-          <div className="row-span-2 w-[80%] justify-self-center rounded-md border border-neutral-800 aspect-square bg-neutral-950">
-            Placeholder Download my CV
-            <p className="bg-neutral-950 text-sm p-3 rounded-md border border-neutral-900 italic">
+          <div className="row-span-2 w-[80%] p-4 justify-self-center rounded-md border border-neutral-800 self-start bg-neutral-950">
+            <div className="flex flex-col gap-3 items-center">
+              <div className="size-30 bg-primary rounded-full"></div>
+              <h3 className="font-bold text-2xl">Giuseppe Crescitelli</h3>
+            </div>
+            <p className="bg-neutral-900 mt-3 text-center text-sm p-3 rounded-md border border-neutral-800 italic">
               Self-taught frontend developer driven by modern technologies,{" "}
               <span className="text-primary">clean</span> and{" "}
               <span className="text-primary">maintainable code</span>, and an
               unrelenting hunger to grow,{" "}
               <span className="text-primary">learn</span>, and succeed.
             </p>
+            <Link href={"/contact"}>
+              <Button className="w-full mt-10 py-2 text-lg">
+                Get in Touch
+              </Button>
+            </Link>
+
+            <Link target="blank" href={links.github} className="w-full">
+              <Button
+                variant="secondary"
+                className="w-full flex justify-center gap-2 mt-4"
+              >
+                <GitHub />
+                GitHub Profile
+              </Button>
+            </Link>
+            <Link target="blank" href={links.linkedin} className="w-full">
+              <Button
+                variant="secondary"
+                className="w-full flex justify-center gap-2 mt-4"
+              >
+                <Linkedin />
+                Linkedin profile
+              </Button>
+            </Link>
           </div>
           <div>
             <Content />
