19a6d4c871abd5959819527caabd41c22b8ca60d
Giuseppe <cresctelli@gmail.com>
2025-11-24 13:59:00 +0100

    Customized buttons UI

--- components/Button.tsx
+++ components/Button.tsx
@@ -16,11 +16,13 @@ function Button({
   children,
   onClick,
 }: Props) {
-  const base = "cursor-pointer rounded-md";
+  const base =
+    "cursor-pointer rounded-md duration-150 focus:outline-none focus:ring-2 focus:ring-offset-3 focus:ring-offset-black";
 
   const variants = {
-    primary: "text-black bg-primary hover:bg-primary-dark",
-    secondary: "text-white border border-neutral-400",
+    primary: "text-black bg-primary hover:bg-primary-dark focus:ring-primary",
+    secondary:
+      "text-white border border-neutral-900 hover:bg-neutral-950 focus:ring-white",
     icon: "aspect-square flex items-center justify-center border border-neutral-800",
   };
 
